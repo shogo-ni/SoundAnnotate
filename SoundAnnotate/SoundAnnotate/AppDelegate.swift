21314699781f57ff908ecb3552f06bc5ad4c1f29
shogo-ni <bmk0105sho44go05@gmail.com>
2016-02-26 09:36:42 +0900

    add Inayamasan

---
+++ SoundAnnotate/SoundAnnotate/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SoundAnnotate
+//
+//  Created by Takahiko Inayama on 2/25/16.
+//  Copyright © 2016 TETRA2000. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SoundAnnotate/SoundAnnotate/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SoundAnnotate
+//
+//  Created by Takahiko Inayama on 2/25/16.
+//  Copyright © 2016 TETRA2000. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
